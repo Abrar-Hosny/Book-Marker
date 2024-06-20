5d4987fe071a2fbfcdf27172c4c628a0155daaf8
Abrar Hosny <109631053+Abrar-Hosny@users.noreply.github.com>
2024-06-20 12:30:34 +0300

    delete a book from the list and update the local storage

--- JS/index.js
+++ JS/index.js
@@ -29,12 +29,21 @@ cartona+=`<tr>
           <td>${i+1}</td>
           <td>${bookList[i].name}</td>
           <td><a class="btn  fw-bold ">Visit</a></td>
-          <td><a  class="btn  fw-bold ">Delete</a></td>
+          <td><a onclick="deleteBook(${i})" class="btn  fw-bold ">Delete</a></td>
           </tr>
        `
   }
 
 displaybody.innerHTML=cartona ; 
+
+}
+
+
+function deleteBook(deletedindex){
+  bookList.splice(deletedindex,1);
+  localStorage.setItem("books" , JSON.stringify(bookList));
+console.log(deletedindex)
+display();
 
 }
 
